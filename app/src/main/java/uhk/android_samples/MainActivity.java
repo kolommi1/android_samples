e8734d6a2ae03084611dbf4e9b503081529ccbfa
kolommi1 <kolommi1@uhk.cz>
2017-12-29 22:50:46 +0100

    Vytvoření OpenGL ES contextu

--- app/src/main/java/uhk/android_samples/MainActivity.java
+++ app/src/main/java/uhk/android_samples/MainActivity.java
@@ -5,9 +5,27 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyGLSurfaceView mGLView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        // The activity must call the GL surface view's onResume() on activity onResume().
+        super.onResume();
+        mGLView.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        // The activity must call the GL surface view's onPause() on activity onPause().
+        super.onPause();
+        mGLView.onPause();
     }
 }
