a29217b95561d3f993c7698f699e640036f8a2e4
kolommi1 <kolommi1@uhk.cz>
2018-01-23 19:00:20 +0100

    První úloha - pročištění kódu, ošetření chybových stavů

--- app/src/main/java/uhk/android_samples/lvl1basic/p00/p01buffer/Renderer.java
+++ app/src/main/java/uhk/android_samples/lvl1basic/p00/p01buffer/Renderer.java
@@ -2,6 +2,7 @@ package uhk.android_samples.lvl1basic.p00.p01buffer;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.util.Log;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -43,16 +44,21 @@ public class Renderer implements GLSurfaceView.Renderer {
         GLES20.glUseProgram(shaderProgram);
 
         bindBuffers();
-        // draw
+
+        // draw - MODE(triangles), number of vertexes, type of vertexes, location of indices
         GLES20.glDrawElements(GLES20.GL_TRIANGLES, 3, GLES20.GL_UNSIGNED_SHORT, 0);
+
+        //unbind from the buffers
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, 0);
     }
 
-    public void createBuffers(){
-// create and fill vertex buffer data
+    private void createBuffers(){
+        // create and fill vertex buffer data
         float[] vertexBufferData = {
-                -1, -1,
-                1, 0,
-                0, 1
+            -1, -1,
+            1, 0,
+            0, 1
         };
         // create buffer required for sending data to a native library
         FloatBuffer vertexBufferBuffer = ByteBuffer.allocateDirect(vertexBufferData.length * 4)
@@ -67,7 +73,6 @@ public class Renderer implements GLSurfaceView.Renderer {
 
         // create and fill index buffer data (element buffer in OpenGL terminology)
         short[] indexBufferData = { 0, 1, 2 };
-
         // create buffer required for sending data to a native library
         ShortBuffer indexBufferBuffer = ByteBuffer.allocateDirect(indexBufferData.length * 2)
                 .order(ByteOrder.nativeOrder()).asShortBuffer();
@@ -76,18 +81,15 @@ public class Renderer implements GLSurfaceView.Renderer {
 
         GLES20.glGenBuffers(1, indexBuffer, 0);
         GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, indexBuffer[0]);
-        GLES20.glBufferData(GLES20.GL_ELEMENT_ARRAY_BUFFER,
-                indexBufferData.length * 2, indexBufferBuffer,
-                GLES20.GL_STATIC_DRAW);
+        GLES20.glBufferData(GLES20.GL_ELEMENT_ARRAY_BUFFER,indexBufferData.length * 2,
+                indexBufferBuffer, GLES20.GL_STATIC_DRAW);
     }
 
-    public void createShaders(){
+    private void createShaders(){
         String shaderVertSrc =
                 "attribute vec2 inPosition;" + // input from the vertex buffer
                 "void main() {" +
-                "	vec2 position = inPosition;" +
-                "   position.x += 0.1;" +
-                " 	gl_Position = vec4(position, 0.0, 1.0);" +
+                " 	gl_Position = vec4(inPosition, 0.0, 1.0);" +
                 "}"
         ;
         // gl_Position - built-in vertex shader output variable containing
@@ -97,36 +99,66 @@ public class Renderer implements GLSurfaceView.Renderer {
         String shaderFragSrc =
                 "precision mediump float;" +
                 "void main() {" +
-                " 	gl_FragColor = vec4(0.5,0.1,0.8, 1.0);" +
+                " 	gl_FragColor = vec4(0.5,0.1,0.8, 1.0);" + //output
                 "}"
         ;
 
-        // vertex shader
+        // vertex shader - create and compile
         int vs = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
-        GLES20.glShaderSource(vs, shaderVertSrc);
-        GLES20.glCompileShader(vs);
-        System.out.println("Compile VS error: " + GLES20.glGetShaderInfoLog(vs) +  GLES20.GL_COMPILE_STATUS) ;
+        if (vs != 0) {
+            GLES20.glShaderSource(vs, shaderVertSrc);
+            GLES20.glCompileShader(vs);
 
-        // fragment shader
+            int[] compiled = new int[1];
+            GLES20.glGetShaderiv(vs, GLES20.GL_COMPILE_STATUS, compiled, 0);
+            if (compiled[0] == 0) {
+                Log.e("OpenGLES_shaders", "Could not compile shader " + GLES20.GL_VERTEX_SHADER + ":");
+                Log.e("OpenGLES_shaders", GLES20.glGetShaderInfoLog(vs));
+                GLES20.glDeleteShader(vs);
+                vs = 0;
+            }
+        }
+
+        // fragment shader - create and compile
         int fs = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
-        GLES20.glShaderSource(fs,  shaderFragSrc);
-        GLES20.glCompileShader(fs);
-        System.out.println("Compile FS error: " +  GLES20.glGetShaderInfoLog(vs) + GLES20.GL_COMPILE_STATUS);
+        if (fs != 0) {
+            GLES20.glShaderSource(fs,  shaderFragSrc);
+            GLES20.glCompileShader(fs);
 
-        // link program
+            int[] compiled = new int[1];
+            GLES20.glGetShaderiv(fs, GLES20.GL_COMPILE_STATUS, compiled, 0);
+            if (compiled[0] == 0) {
+                Log.e("OpenGLES_shaders", "Could not compile shader " + GLES20.GL_FRAGMENT_SHADER + ":");
+                Log.e("OpenGLES_shaders", GLES20.glGetShaderInfoLog(fs));
+                GLES20.glDeleteShader(fs);
+                fs = 0;
+            }
+        }
+
+        // create and link program
         shaderProgram = GLES20.glCreateProgram();
-        GLES20.glAttachShader(shaderProgram, vs);
-        GLES20.glAttachShader(shaderProgram, fs);
-        GLES20.glLinkProgram(shaderProgram);
-        System.out.println("Link error: " + GLES20.glGetProgramInfoLog(shaderProgram) +  GLES20.GL_LINK_STATUS);
+        if (shaderProgram != 0) {
+            GLES20.glAttachShader(shaderProgram, vs);
+            GLES20.glAttachShader(shaderProgram, fs);
+            GLES20.glLinkProgram(shaderProgram);
 
-        if (vs > 0) GLES20.glDetachShader(shaderProgram, vs);
-        if (fs > 0) GLES20.glDetachShader(shaderProgram, fs);
-        if (vs > 0) GLES20.glDeleteShader(vs);
-        if (fs > 0) GLES20.glDeleteShader(fs);
+            int[] linkStatus = new int[1];
+            GLES20.glGetProgramiv(shaderProgram, GLES20.GL_LINK_STATUS, linkStatus, 0);
+            if (linkStatus[0] != GLES20.GL_TRUE) {
+                Log.e("OpenGLES_shaders", "Could not link program: ");
+                Log.e("OpenGLES_shaders", GLES20.glGetProgramInfoLog(shaderProgram));
+                GLES20.glDeleteProgram(shaderProgram);
+                shaderProgram = 0;
+            }
+
+            if (vs > 0) GLES20.glDetachShader(shaderProgram, vs);
+            if (fs > 0) GLES20.glDetachShader(shaderProgram, fs);
+            if (vs > 0) GLES20.glDeleteShader(vs);
+            if (fs > 0) GLES20.glDeleteShader(fs);
+        }
     }
 
-    void bindBuffers() {
+    private void bindBuffers() {
         // internal OpenGL ID of a vertex shader input variable
         int locPosition = GLES20.glGetAttribLocation(shaderProgram, "inPosition");
 
@@ -135,10 +167,11 @@ public class Renderer implements GLSurfaceView.Renderer {
         // - describe how many components of which type correspond to it in the
         // data, how large is one vertex (its stride in bytes) and at which byte
         // of the vertex the first component starts
-        // 2 components, of type float, do not normalize (convert to [0,1]),
-        // vertex of 8 bytes, start at the beginning (byte 0)
+        // 2 components(2 coordinates per vertex), of type float, do not normalize (convert to [0,1]),
+        // vertex of 8 bytes( 2*4 - numberOfCoordinates*bytesPerFloat ), start at the beginning (byte 0)
         GLES20.glVertexAttribPointer(locPosition, 2, GLES20.GL_FLOAT, false, 8, 0);
         GLES20.glEnableVertexAttribArray(locPosition);
+
         GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, indexBuffer[0]);
     }
 
