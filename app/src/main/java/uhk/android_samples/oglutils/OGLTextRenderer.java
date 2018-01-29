5cddefd722bf7e1938b46c585c8dfae8e1502fb0
kolommi1 <kolommi1@uhk.cz>
2018-01-29 15:59:41 +0100

    Project sync

--- app/src/main/java/uhk/android_samples/oglutils/OGLTextRenderer.java
+++ app/src/main/java/uhk/android_samples/oglutils/OGLTextRenderer.java
@@ -1,101 +1,64 @@
-package oglutils;
+package uhk.android_samples.oglutils;
 
-import java.awt.Color;
-import java.awt.Font;
 
-import com.jogamp.opengl.GL2GL3;
-import com.jogamp.opengl.util.awt.TextRenderer;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
 
 public class OGLTextRenderer {
-	private final GL2GL3 gl;
-	private final TextRenderer renderer;
 	private int width;
 	private int height;
-	private Color color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-	
-	
-	/**
-	 * Create TextRenderer object
-	 * 
-	 * @param width
-	 *            width of output rendering frame
-	 * @param height
-	 *            height of output rendering frame
-	 * @param font
-	 * 			  font
-	 */
-	public OGLTextRenderer(GL2GL3 gl, int width, int height, Font font) {
-		this.gl = gl;
-		this.width = width;
-		this.height = height;
-		renderer = new TextRenderer(font);
-	}
 
-	/**
-	 * Create TextRenderer object
-	 * 
-	 * @param width
-	 *            width of output rendering frame
-	 * @param height
-	 *            height of output rendering frame
-	 */
-	public OGLTextRenderer(GL2GL3 gl, int width, int height) {
-		this(gl, width, height, new Font("SansSerif", Font.PLAIN, 12));
-	}
-
-	/**
-	 * Update size of output rendering frame
-	 * 
-	 * @param width
-	 *            updated width of output rendering frame
-	 * @param height
-	 *            updated height of output rendering frame
-	 */
-	public void updateSize(int width, int height) {
-		this.width = width;
-		this.height = height;
-	}
-
-	/**
-	 * Changes the current color. The default color is opaque white.
-	 * 
-	 * @param color
-	 *            the new color to use for rendering text
-	 */
-	public void setColor(Color color) {
-		this.color = color;
-	}
-	 
 	/**
 	 * Draw string on 2D coordinates of the raster frame
-	 * 
-	 * @param gl
-	 * @param x
-	 *            x position of string in range <0, width-1> of raster frame
-	 * @param y
-	 *            y position of string in range <0, height-1> of raster frame
+	 *
 	 * @param s
 	 *            string to draw
 	 */
-	public void drawStr2D(int x, int y,	String s) {
-		if (renderer == null || s == null)
-			return;
-		gl.glUseProgram(0);
-		gl.glViewport(0, 0, width, height);
-		gl.glPolygonMode(GL2GL3.GL_FRONT_AND_BACK, GL2GL3.GL_FILL);
-		gl.glActiveTexture(GL2GL3.GL_TEXTURE0);
-		
-		renderer.setColor(color);
+	public static void drawStr2D(int x, int y, int width, int height,	String s, int color) {
+		// Create an empty, mutable bitmap
+		Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		// get a canvas to paint over the bitmap
+		Canvas canvas = new Canvas(bitmap);
+		bitmap.eraseColor(0);
 
-		renderer.beginRendering(width, height);
-		renderer.draw(s, x, y);
-		
-		renderer.endRendering();
+		// get a background image from resources
+		// note the image format must match the bitmap format
+	/*	Drawable background = Drawable.create; context.getResources().getDrawable(R.drawable.background);
+		background.setBounds(0, 0, x, y);
+		background.draw(canvas); // draw the background to our bitmap*/
+
+		// Draw the text
+		Paint textPaint = new Paint();
+		textPaint.setTextSize(32);
+		textPaint.setAntiAlias(true);
+		textPaint.setColor(color);
+		// draw the text centered
+		canvas.drawText(s, x,y, textPaint);
+
+		int[] textures = new int[1];
+		//Generate one texture pointer...
+		GLES20.glGenTextures(1, textures, 0);
+		//...and bind it to our array
+		GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textures[0]);
+
+		//Create Nearest Filtered Texture
+		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+
+		//Different possible texture parameters, e.g. GL10.GL_CLAMP_TO_EDGE
+		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_REPEAT);
+		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_REPEAT);
+
+		//Use the Android GLUtils to specify a two-dimensional texture image from our bitmap
+		GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+
+		//Clean up
+		bitmap.recycle();
 	}
 
-	@Override
-	public void finalize() throws Throwable{
-		super.finalize();
-		renderer.dispose();
-	}
 }
